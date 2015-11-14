51cea413270a21d4c4aa3d8eba61b2083387dc41
David Abrahams <david.abrahams@students.olin.edu>
2015-11-14 16:01:01 -0500

    created build files and finished instruciton decoder

--- verilog/instructionDecoder.v
+++ verilog/instructionDecoder.v
@@ -5,21 +5,20 @@
 
 module instructionDecoder
 (
-    input reg [31:0]    instruction_in,
-    output reg [5:0]    op,
-    output reg [4:0]    rs,
-    output reg [4:0]    rt,
-    output reg [4:0]    rd,
-    output reg [15:0]   imm_16,
-    output reg [25:0]   address_26,
+    input [31:0] instruction_in,
+    output [5:0]     op,
+    output [4:0]     rs,
+    output [4:0]     rt,
+    output [4:0]     rd,
+    output [15:0]    imm_16,
+    output [25:0]   address_26
 );
 
-
-  assign op <= instruction_in[31:26];
-  assign rs <= instruction_in[25:21];
-  assign rt <= instruction_in[20:16];
-  assign rd <= instruction_in[15:11];
-  assign imm_16 <= instruction_in[15:0];
-  assign address_26 <= instruction_in[25:0];
+  assign op = instruction_in[31:26];
+  assign rs = instruction_in[25:21];
+  assign rt = instruction_in[20:16];
+  assign rd = instruction_in[15:11];
+  assign imm_16 = instruction_in[15:0];
+  assign address_26 = instruction_in[25:0];
 
 endmodule
